9b0361422a21221560814f090e7ca1f7644e1adb
Sandro Speh <sandro.speh@student.hpi.de>
2021-09-05 14:24:12 +0200

    implemented new chart horizontalStackedBarChart

---
+++ visualizations/horizontal_stacked_bar_chart.js
@@ -0,0 +1,62 @@
+import MultipleDatasetChart from './multiple_dataset_chart.js'
+import BarChart from './bar_chart.js'
+
+import '../external/chartjs-plugin-datalabels.js'
+
+export default class HorizontalStackedBarChart extends MultipleDatasetChart {
+    constructor(parameters = { canvas: null, statistics_container: null }) {
+        super(parameters)
+    }
+
+    _construct_chart_plugins() {
+        return [ChartDataLabels]
+    }
+
+    _construct_chart_dataset(dataset, index, nr_of_datasets) {
+        const dataset_object = super._construct_chart_dataset(dataset, index, nr_of_datasets)
+
+        const color_base =
+            `rgba(0,` +
+            `${(255 / nr_of_datasets) * (nr_of_datasets - index)},` +
+            `${(255 / nr_of_datasets) * index},`
+
+        dataset_object.type = 'bar'
+        dataset_object.borderColor = `${color_base}1)`
+        dataset_object.backgroundColor = `${color_base}0.25)`
+        dataset_object.hoverBackgroundColor = `${color_base}1)`
+        dataset_object.borderWidth = 2
+        dataset_object.datalabels = {
+            color: '#000000',
+            anchor: 'center',
+            align: 'center',
+            formatter: (value) => ''
+        }
+
+        return dataset_object
+    }
+
+    _construct_chart_options() {
+        return {
+            indexAxis: 'y',
+            plugins: {
+                title: {
+                    text: this.title,
+                    display: true
+                }
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    stacked: true
+                },
+                x: {
+                    stacked: true
+                }
+            },
+            onClick: (event, clickedElements, chart) => {
+                // visit url
+            }
+        }
+    }
+}
