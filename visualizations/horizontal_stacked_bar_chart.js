d7a5ddaac504dc82a1b02681c227d4b2a9ea7f1b
Sandro Speh <sandro.speh@student.hpi.de>
2021-09-07 10:08:01 +0200

    implemented usefull tooltip and changed color

--- visualizations/horizontal_stacked_bar_chart.js
+++ visualizations/horizontal_stacked_bar_chart.js
@@ -22,21 +22,16 @@ export default class HorizontalStackedBarChart extends MultipleDatasetChart {
 
         const dataset_object = super._construct_chart_dataset(dataset, index, nr_of_datasets)
 
-        const color_base =
-            `rgba(0,` +
-            `${(255 / nr_of_datasets) * (nr_of_datasets - index)},` +
-            `${(255 / nr_of_datasets) * index},`
-
         dataset_object.type = 'bar'
-        dataset_object.borderColor = `${color_base}1)`
-        dataset_object.backgroundColor = `${color_base}0.25)`
-        dataset_object.hoverBackgroundColor = `${color_base}1)`
+        dataset_object.borderColor = 'rgba(76, 175, 80, 1)'
+        dataset_object.backgroundColor = 'rgba(76, 175, 80, 0.25)'
+        dataset_object.hoverBackgroundColor = 'rgba(76, 175, 80, 1)'
         dataset_object.borderWidth = 2
         dataset_object.datalabels = {
             color: '#000000',
             anchor: 'center',
             align: 'center',
-            formatter: (value) => ''
+            formatter: (value) => (value === 0 ? '' : `${value}`)
         }
 
         return dataset_object
@@ -59,18 +54,26 @@ export default class HorizontalStackedBarChart extends MultipleDatasetChart {
                     yAlign: 'bottom',
                     xAlign: 'center',
                     callbacks: {
-                        title: (item, data) => {
-                            const dataObject = this._data[item[0].dataIndex][item[0].datasetIndex]
-                            return dataObject.title.length > 50
-                                ? `${dataObject.title.slice(0, 50)}...`
-                                : dataObject.title
+                        title: (item) => {
+                            const data_object = this._data[item[0].dataIndex][item[0].datasetIndex]
+                            const data_object_id = data_object.url.substring(
+                                data_object.url.lastIndexOf('/') + 1
+                            )
+                            return `#${data_object_id}: ${
+                                data_object.title.length > 50
+                                    ? `${data_object.title.slice(0, 50)}...`
+                                    : data_object.title
+                            }`
                         },
                         label: (item) => {
-                            debugger
-                            const dataObject = this._data[item.dataIndex][item.datasetIndex]
-                            return dataObject.body.length > 50
-                                ? `${dataObject.body.slice(0, 50)}...`
-                                : dataObject.body
+                            const data_object = this._data[item.dataIndex][item.datasetIndex]
+                            const value_string = `${this._data_title}: ${data_object.value}`
+                            return [
+                                value_string,
+                                data_object.body.length > 50
+                                    ? `${data_object.body.slice(0, 50)}...`
+                                    : data_object.body
+                            ]
                         }
                     }
                 }
@@ -85,7 +88,7 @@ export default class HorizontalStackedBarChart extends MultipleDatasetChart {
                     stacked: true
                 }
             },
-            onClick: (event, clickedElements, chart) => {
+            onClick: (event, clickedElements) => {
                 const stack_index = clickedElements[0].index
                 const index = clickedElements[0].datasetIndex
                 const object = this.data[stack_index][index]
